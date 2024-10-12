241bb5a8c23702c106bf21cd592e2fd1c5466049
Ramburganov <136573935+Ramburganov@users.noreply.github.com>
2024-10-12 22:26:35 +0300

    Update Park.java

--- Park.java
+++ Park.java
@@ -1,1 +1,30 @@
+public class Park {
+    private String name;
 
+    public Park(String name) {
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Парк: " + name);
+    }
+
+  
+    public static class Attraction {
+        private String name;
+        private int cost;
+        private String workTime;
+
+        public Attraction(String name, int cost, String workTime) {
+            this.name = name;
+            this.cost = cost;
+            this.workTime = workTime;
+        }
+
+        public void printInfo() {
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Стоимость: " + cost);
+            System.out.println("Время работы: " + workTime);
+        }
+    }
+}
