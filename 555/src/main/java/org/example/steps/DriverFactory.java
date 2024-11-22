f0a1d8208a4cb01a8fb4cf1cf46bde533bf817b5
RamBurgan <burganovramks@gmail.com>
2024-11-23 00:02:47 +0300

    Lesson_16.1

---
+++ 555/src/main/java/org/example/steps/DriverFactory.java
@@ -0,0 +1,15 @@
+package org.example.steps;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
